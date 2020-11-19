17f68449c64a87f1d80c73b923fc8760595624e2
aejazballari <aejazballari@gmail.com>
2020-11-19 23:39:44 +0530

    completed

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const navSlide  = () => {
-    const burger = document.querySelector(".burger-container");
-    const nav = document.querySelector(".nav-links")
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger-container");
+const navlinks = document.querySelectorAll(".nav-links");
+const link = document.querySelector(".links");
 
+const navSlide  = () => {
     burger.addEventListener("click", () => {
         nav.classList.toggle("links-container");
          nav.classList.toggle("show-links");
     });
 };
 
-// const closeNav = (remove) => {
-//     const close = document.querySelectorAll(".close");
-//     const nav = document.querySelector(".navlinks");
+//   const shut = () => {
+//     navlinks.addEventListener("click", (e) => {
+//         const close = e.Target.parenetElement;
+//         console.log(close);
+//         close.parenetElement.classList.toggle("links-container");
+//         close.classList.toggle("show-links");
+//     })
+//   } 
+//     // console.log(shut);
+    // const close = e.currentTarget.parentElement;
+ 
+    navlinks.forEach(() => {
+        link.addEventListener("click", () => {
+            navlinks.classList.toggle("links-container")
+        })
+        
+    })
+
     
-//     close.forEach((click) => {
-//         click.addEventListener("click", () => {
-//             nav.classList.remove("links-container");
-//         });
-//         return click;
-//     });
-//     return remove;
-//     }
-// closeNav();
 
 navSlide();
+// shut();
+
+
