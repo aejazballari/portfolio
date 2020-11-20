0df3b7cc3c242304f0a61f78d45b8b91ad4a8364
aejazballari <aejazballari@gmail.com>
2020-11-20 17:34:27 +0530

    collapsing nav done

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
-const nav = document.querySelector(".nav-links");
+const nav = document.querySelector(".nav-container");
 const burger = document.querySelector(".burger-container");
 const navlinks = document.querySelectorAll(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+const animateLinks = () => {
+     links.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = "";
+            } else {
+                link.style.animation =  `fadelinks 0.5s ease forwards ${index / 3 + 1.2}s`;
+                }
+         });
+    }
 
 const navSlide  = () => {
     burger.addEventListener("click", () => {
-        nav.classList.toggle("links-container");
-         nav.classList.toggle("show-links");
+        nav.classList.toggle("show-links");
+        animateLinks();
+
     });
-};
+   
+    };
 
 navSlide();
 
+const close = () =>{
+    navlinks.forEach((link)=>{
+        link.addEventListener("click", ()=>{
+            nav.classList.toggle("show-links");
+            animateLinks();
+        })
+    })
+}
 
+close();
+
+
+ 
+// links.forEach((click)=>{
+//     click.addEventListener("click", ()=>{
+//         close();
+//     })   
+// })
         
- navlinks.forEach( (click) => {
-     click.addEventListener("click", () => {
-        click.classList.remove("links-container");
-        click.classList.remove("show-links");
-     })
-})
+//  navlinks.forEach( (click) => {
+//      click.addEventListener("click", () => {
+//          console.log(click);
+//         // click.classList.remove("show-links");
+//      })
+// })
 
 
 
