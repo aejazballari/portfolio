c6bb84fb0c5fcbcdf143830335edf84bc0bf922b
aejazballari <aejazballari@gmail.com>
2020-11-20 12:13:42 +0530

    nav toggle done

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const nav = document.querySelector(".nav-links");
-const burger = document.querySelector(".burger-container");
-const navlinks = document.querySelectorAll(".nav-links");
+// const nav = document.querySelector(".nav-links");
+// const burger = document.querySelector(".burger-container");
+// const navlinks = document.querySelectorAll(".nav-links");
 
-const navSlide  = () => {
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("links-container");
-         nav.classList.toggle("show-links");
-    });
-};
+// const navSlide  = () => {
+//     burger.addEventListener("click", () => {
+//         nav.classList.toggle("links-container");
+//          nav.classList.toggle("show-links");
+//     });
+// };
 
-navSlide();
+// navSlide();
 
 
         
- navlinks.forEach( (click) => {
-     click.addEventListener("click", () => {
-        click.classList.remove("links-container");
-        click.classList.remove("show-links");
-     })
-})
+//  navlinks.forEach( (click) => {
+//      click.addEventListener("click", () => {
+//         click.classList.remove("links-container");
+//         click.classList.remove("show-links");
+//      })
+// })
 
 
 
